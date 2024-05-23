415d8486dd3d3d2766674cd44aa7bc69cafb38a4
Raphael Castro <rafaphcastro@gmail.com>
2024-05-23 04:20:30 +0000

    feat: settings are now lazy evaluated

--- src/common/settings.py
+++ src/common/settings.py
@@ -1,17 +1,37 @@
-from pydantic import PostgresDsn, ValidationError
-from pydantic_settings import BaseSettings
+import os
+
+from pydantic import BaseModel, PostgresDsn, ValidationError
 
 
-class Settings(BaseSettings):
+class InvalidSettingsError(Exception):
+    def __init__(self, msg: str = "Invalid settings") -> None:
+        super().__init__(msg)
+
+
+class BaseSettings:
     DATABASE_URL: PostgresDsn
     SECRET_KEY: str
     ALGORITHM: str
     EXPIRATION_TIME_MINUTES: int
 
 
-try:
-    settings = Settings()
-except ValidationError as error:  # pragma: no cover
-    print(error.json(indent=4))
-    msg = "Invalid settings"
-    raise Exception(msg) from error
+class Settings(BaseSettings, BaseModel): ...
+
+
+class LazySettings(BaseSettings):
+    def __init__(self) -> None:
+        self._settings: Settings | None = None
+
+    def _initialize(self) -> None:
+        try:
+            self._settings = Settings.model_validate(os.environ)
+        except ValidationError as error:
+            raise InvalidSettingsError() from error
+
+    def __getattr__(self, name: str) -> object:
+        if self._settings is None:
+            self._initialize()
+        return getattr(self._settings, name)
+
+
+settings = LazySettings()
