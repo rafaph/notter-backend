d3e18259bd5106d137f2b238db0b968874a99aea
Raphael Castro <2117949+rafaph@users.noreply.github.com>
2024-05-26 03:51:13 +0000

    chore: refactor devcontainer configuration

--- .devcontainer/pdm-shell/install.sh
+++ .devcontainer/pdm-shell/install.sh
@@ -11,20 +11,6 @@ if [  -z "$_REMOTE_USER" ] || [ -z "$_REMOTE_USER_HOME" ]; then
   exit 1
 fi
 
-# Set pdm func for bash
-cat << EOF >> "$_REMOTE_USER_HOME/.bashrc"
-pdm() {
-  local command=\$1
-
-  if [[ "\$command" == "shell" ]]; then
-      eval \$(pdm venv activate)
-  else
-      command pdm \$@
-  fi
-}
-EOF
-chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.bashrc
-
 # Set pdm func for zsh
 cat << EOF >> "$_REMOTE_USER_HOME/.zshrc"
 pdm() {
@@ -38,17 +24,3 @@ pdm() {
 }
 EOF
 chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.zshrc
-
-mkdir -p $_REMOTE_USER_HOME/.config/fish
-cat << EOF >> "$_REMOTE_USER_HOME/.config/fish/config.fish"
-function pdm
-    set cmd \$argv[1]
-
-    if test "\$cmd" = "shell"
-        eval (pdm venv activate)
-    else
-        command pdm \$argv
-    end
-end
-EOF
-chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.config/
